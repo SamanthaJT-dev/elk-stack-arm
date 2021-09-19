a21d27cc652f0818b81739eeb56c4f48688aa19c
Diego Alloza González <diego.allozagonzalez.practicas@telefonica.com>
2021-09-19 16:32:47 +0200

    chore: dbug elasticsearch

--- elasticsearch/elastic_start.sh
+++ elasticsearch/elastic_start.sh
@@ -6,4 +6,8 @@ mkdir -p "${ES_LOG_DIR}"
 mkdir -p "${ES_DATA_DIR}"
 mkdir -p "${ES_TMP_DIR}"
 
+cat /usr/share/elasticsearch/es_start.sh
+
+echo starting elasticsearch
 /usr/share/elasticsearch/es_start.sh
+echo this is the end
